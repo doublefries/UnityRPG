c387208ad564b8b857d3b6ab38188842f0ce6453
Jason-Adogah <jadogah@uwo.ca>
2026-03-19 16:06:22 -0400

    Initial Player Movement
    implement basic 2D top-down player movement
    
    Added PlayerController script with horizontal/vertical input handling.
    
    Prepared script for future sprite/animation integration.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private float speedX, speedY;
+    Rigidbody2D rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speedX = Input.GetAxis("Horizontal") *  moveSpeed;
+        float speedY = Input.GetAxis("Vertical") *  moveSpeed;
+        
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
