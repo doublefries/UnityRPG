520bcfe6bff5c5ad225e001c9dce66e4207ac183
Samuel Pipher-Swyer <spiphers@uwo.ca>
2026-03-24 11:24:54 -0400

    Editing the IAttackable interface

--- Assets/Interfaces/IAttackable.cs
+++ Assets/Interfaces/IAttackable.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public Interface IAttackable
 {
-    
+    // how much attack damage is dealt per hit
+    int AttackDamage { get; }
+    // range of hitbox that can actually be used to deal damage
+    float AttackRange { get; }
+    // intervals between attacks/hits 
+    float AttackCooldown { get; }
+    // enemy can attack player, object, or other enemy, may change this
+    // attacker depends on a abstraction
+    void Attack(IDamageable target);
 }
