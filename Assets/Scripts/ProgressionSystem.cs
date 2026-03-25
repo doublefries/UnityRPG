0dae8cac0bf2c84f847dc86b8c3a489e8e3f4dee
Hareshs7 <hsubram@uwo.ca>
2026-03-25 01:12:54 -0400

    Added Methods for Total Ingredients , Collecting coins.

--- Assets/Scripts/ProgressionSystem.cs
+++ Assets/Scripts/ProgressionSystem.cs
@@ -5,10 +5,30 @@ public class ProgressionSystem : MonoBehaviour
     //Variables 
     public int coins = 0;
     public int currentLevel = 1;
+    public int ingredientsCollected = 0;
+    public int totalIngredients = 5;
 
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+    }
+
+    public void CollectIngredient()
+    {
+        ingredientsCollected++;
+    }
+
+    public bool AllIngredientsCollected()
+    {
+        return ingredientsCollected >= totalIngredients;
+    }
+    
     public void CompleteLevel()
     {
-        currentLevel += 1;
+        currentLevel++;
+        
+        //Print in the console 
+        Debug.Log("Level completed, Currently at level " + currentLevel);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
