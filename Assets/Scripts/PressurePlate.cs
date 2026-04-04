4eb61989bc20030e0bcbcc729a52cd1e46934bd5
Jason-Adogah <jadogah@uwo.ca>
2026-04-04 15:13:48 -0400

    Completed PressurePlates
    Created three pressure plate objects that  are each connected to a vine wall.
    
    When the player interacts with them, the associated vined wall is removed.

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -15,6 +15,25 @@ public class PressurePlate : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (_activated) //If already activated do nothing
+        {
+            return;
+        }
+
+        _activated = true;
+
+        if (_sr != null && pressedSprite != null) //Visually change the sprite once its pressed
+        {
+            _sr.sprite = pressedSprite;
+        }
+
+        if (linkedWall != null)
+        {
+            linkedWall.RemoveWall();
+        }
+        else
+        {
+            Debug.Log("No VinedWall linked to this pressure plate.");
+        }
     }
 }
