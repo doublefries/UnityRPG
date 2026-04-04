eeb9ea885f2b78ec2a193b0206f5f788854adf0a
Hareshs7 <hsubram@uwo.ca>
2026-04-04 13:17:06 -0400

    Coins Implemented

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
