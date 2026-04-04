7dab04656f65c2d9514eb8323db4aad0ec2acab0
Jason-Adogah <jadogah@uwo.ca>
2026-04-04 13:43:44 -0400

    Added VinedWall and PressurePlate
    Created the scripts for the VinedWall and PressurePlates
    
    Also added a PressurePlate tag to the object

---
+++ Assets/Scripts/VinedWall.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class VinedWall : MonoBehaviour
+{
+    [SerializeField] private Tilemap wallTilemap;
+    [SerializeField] private Vector3Int[] tilesToRemove; //cells to clear
+
+
+    public void RemoveWall()
+    {
+        
+    }
+    
+}
