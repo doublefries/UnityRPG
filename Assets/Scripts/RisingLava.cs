0167438e910888c8960c8bba2812058bee7aaee2
Nurnzy <turnerreucassel5@gmail.com>
2026-04-06 16:58:57 -0400

    feature:max height on lava; can't get you if on final platform (win)

--- Assets/Scripts/RisingLava.cs
+++ Assets/Scripts/RisingLava.cs
@@ -5,9 +5,11 @@ public class RisingLava: MonoBehaviour
     [SerializeField] private float initialRiseSpeed = 0.4f;
     [SerializeField] private float exponentialRate = 0.15f;
     [SerializeField] private float maxRiseSpeed = 4f;
+    [SerializeField] private float maxYPosition = 15f;
 
     private Rigidbody2D rb;
     private float elapsedTime;
+    private bool reachedMaxHeight = false; //fields to be used
 
     private void Awake()
     {
@@ -16,12 +18,22 @@ public class RisingLava: MonoBehaviour
 
     private void FixedUpdate() //make lava increase speed exponentially as level continues
     {
+        if (reachedMaxHeight) return;
+        
         elapsedTime += Time.fixedDeltaTime;
 
         float currentSpeed = initialRiseSpeed * Mathf.Exp(exponentialRate * elapsedTime);
         currentSpeed = Mathf.Min(currentSpeed, maxRiseSpeed);
 
-        rb.MovePosition(rb.position + Vector2.up * currentSpeed * Time.fixedDeltaTime);
+        Vector2 nextPosition = rb.position + Vector2.up * currentSpeed * Time.fixedDeltaTime; //calculate next position and see if we have reached it
+        
+        if (nextPosition.y >= maxYPosition)
+        {
+            nextPosition.y = maxYPosition;
+            reachedMaxHeight = true;
+        }
+
+        rb.MovePosition(nextPosition);
     }
     
     
