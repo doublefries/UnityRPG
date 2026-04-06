7317b362be86dcd29c8c23d56e44c71079f8204f
Nurnzy <turnerreucassel5@gmail.com>
2026-04-06 13:38:44 -0400

    added exponential increase to lava speed

--- Assets/Scripts/RisingLava.cs
+++ Assets/Scripts/RisingLava.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 public class RisingLava: MonoBehaviour
 {
-    [SerializeField] private float riseSpeed = 0.75f;
+    [SerializeField] private float initialRiseSpeed = 0.4f;
+    [SerializeField] private float exponentialRate = 0.15f;
+    [SerializeField] private float maxRiseSpeed = 4f;
 
     private Rigidbody2D rb;
+    private float elapsedTime;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() //make lava increase speed exponentially as level continues
     {
-        rb.MovePosition(rb.position + Vector2.up * riseSpeed * Time.fixedDeltaTime);
+        elapsedTime += Time.fixedDeltaTime;
+
+        float currentSpeed = initialRiseSpeed * Mathf.Exp(exponentialRate * elapsedTime);
+        currentSpeed = Mathf.Min(currentSpeed, maxRiseSpeed);
+
+        rb.MovePosition(rb.position + Vector2.up * currentSpeed * Time.fixedDeltaTime);
     }
     
     
